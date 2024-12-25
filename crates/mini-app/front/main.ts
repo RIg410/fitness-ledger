e8b26532bef4e0d3d403a13244b98fd96f4f2897
rig410 <dmitryyakushev@dfinance.co>
2024-12-25 19:50:45 +0300

    .

--- crates/mini-app/front/main.ts
+++ crates/mini-app/front/main.ts
@@ -1,3 +1,25 @@
+import { tg_init, showPopup, close } from "./tg.js";
+import { auth } from "./auth.js";
+
 async function run() {
     console.log("Starting...");
+    tg_init();
+
+    try {
+        await auth();
+    } catch (e) {
+        console.error("Failed to auth", e);
+        showPopup('Произошла ошибка при авторизаци. Попробуйте перезагрузить страницу.', [
+            { id: 'ok', type: 'default', text: 'Ок' },
+        ], function (btn) {
+            if (btn === 'ok') {
+                close();
+                return;
+            }
+        });
+    }
+
+    
 }
+
+window.addEventListener("load", run);
